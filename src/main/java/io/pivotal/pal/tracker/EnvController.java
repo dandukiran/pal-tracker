ba9e98eb84923180cd8376a60ceca4003ba53738
Kiran Dandu <kiran.dandu@perficient.com>
2018-02-20 18:23:55 -0500

    Adding the env settings

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,40 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+    private final String port;
+    private final String memoryLimit;
+    private final String cfInstanceIndex;
+    private final String cfInstanceAddress;
+
+    public EnvController(@Value("${PORT:NOT SET}") String port,
+                         @Value("${MEMORY_LIMIT:NOT SET}") String memoryLimit,
+                         @Value("${CF_INSTANCE_INDEX:NOT SET}") String cfInstanceIndex,
+                         @Value("${CF_INSTANCE_ADDR:NOT SET}") String cfInstanceAddress) {
+        this.port = port;
+        this.memoryLimit = memoryLimit;
+        this.cfInstanceIndex = cfInstanceIndex;
+        this.cfInstanceAddress = cfInstanceAddress;
+    }
+
+    @GetMapping("/env")
+    public Map getEnv() {
+        Map<String,String> envMap = new HashMap();
+
+        envMap.put("PORT",this.port);
+        envMap.put("MEMORY_LIMIT", this.memoryLimit);
+        envMap.put("CF_INSTANCE_INDEX",this.cfInstanceIndex);
+        envMap.put("CF_INSTANCE_ADDR", this.cfInstanceAddress);
+
+        return envMap;
+    }
+}
+
